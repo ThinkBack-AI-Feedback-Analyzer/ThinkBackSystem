b4330bed4f15214844bfa0a8977630f2447e0c7e
SanthosN8 <santhos346@gmail.com>
2026-03-15 18:50:50 +0530

    AI Models are created for project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import CourseCreatePage from './pages/CourseCreatePage'
 import CreateCourseIntroPage from './pages/CreateCourseIntroPage'
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import StudentPortal from "./pages/StudentPortal";
+import FormCreatePage from "./pages/FormCreatePage";
 
 function App() {
   return (
@@ -25,6 +27,8 @@ function App() {
         <Route path="/course-create" element={<CourseCreatePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/portal" element={<StudentPortal />} />
+        <Route path="/feedbackForm" element={<FormCreatePage />} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
