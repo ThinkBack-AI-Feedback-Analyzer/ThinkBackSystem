961bcb54adeff0a3083276b991b3cf5ef303c880
Piratheesh <piratheeshsai@gmail.com>
2026-03-15 22:03:03 +0530

    app.js updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import CourseCreatePage from './pages/CourseCreatePage'
 import CreateCourseIntroPage from './pages/CreateCourseIntroPage'
@@ -8,18 +8,23 @@ import InstitutionDashboardPage from "./pages/InstitutionDashboardPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route
-        path="/create-course-intro"
-        element={<CreateCourseIntroPage />}
-      />
-      <Route path="/course-create" element={<CourseCreatePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/institution-dashboard" element={<InstitutionDashboardPage />} />
-    </Routes>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        <Route
+          path="/create-course-intro"
+          element={<CreateCourseIntroPage />}
+        />
+
+        <Route path="/course-create" element={<CourseCreatePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/institution-dashboard" element={<InstitutionDashboardPage />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
