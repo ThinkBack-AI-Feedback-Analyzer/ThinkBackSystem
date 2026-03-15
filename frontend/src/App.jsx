a5a208d897add0271a83ccda5eace6d13c022be5
sajeethamm <s92065405@ousl.lk>
2026-03-15 12:43:21 +0530

    Added login and register pages with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import CourseCreatePage from './pages/CourseCreatePage'
 import CreateCourseIntroPage from './pages/CreateCourseIntroPage'
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -12,7 +14,10 @@ function App() {
         element={<CreateCourseIntroPage />}
       />
       <Route path="/course-create" element={<CourseCreatePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
+    
   )
 }
 
