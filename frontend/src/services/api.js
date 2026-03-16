42f1e26b997eeb6a14801d468c905d3e4e1487bf
Piratheesh <piratheeshsai@gmail.com>
2026-03-16 10:32:58 +0530

    workable login

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -18,4 +18,14 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+// Simplify response data
+api.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
